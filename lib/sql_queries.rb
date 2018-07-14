79a7d39d85a0e3145e534f2ac0c7c0dd367352df
Lorena <lorena.giddens@outlook.com>
2018-07-14 01:13:18 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,6 @@ def selects_all_bears_names_and_orders_in_alphabetical_order
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
-  binding.pry
   "SELECT name, age FROM bears WHERE alive = 1 GROUP BY age"
 end
 
